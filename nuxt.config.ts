ba2829f0ebc31532acede295e28173516e2f8565
Pamarelcia Acidmane <pacificteam47@gmail.com>
2026-04-20 23:05:15 +0300

    Fix OAuth bug.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -78,7 +78,7 @@ export default defineNuxtConfig({
       cookie: {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        sameSite: 'lax',
         maxAge: 60 * 60 * 24 * 30
       }
     },
